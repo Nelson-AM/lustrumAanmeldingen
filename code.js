7507ea3f9fb546d8908b1a6f38957264a62ed335
Nelson <Nelson-AM@users.noreply.github.com>
2022-10-10 22:55:29 +0200

    Create code.js

---
+++ code.js
@@ -0,0 +1,7 @@
+function runOnLoad() {
+   var c = document.getElementById("myCanvas");
+   var ctx = c.getContext("2d");
+   ctx.clearRect(0,0,800,600);
+   ctx.fillStyle = "red";
+   ctx.fillRect(10,10,100,100);
+}
